f2d8ba72af77cfea4cd571804e406c7489fa38c1
Alexander Sokol <sokol@compatibl.com>
2023-04-01 00:44:18 -0400

    Add DeletedRecord as delete marker.
    (cherry picked from commit 3dadea8ede04b11d9125e696e50163447830b71b)

--- cl/runtime/__init__.py
+++ cl/runtime/__init__.py
@@ -1,5 +1,6 @@
 from cl.runtime.storage.cl_context import ClContext as Context
 from cl.runtime.storage.cl_record import ClRecord as Record
+from cl.runtime.storage.cl_deleted_record import ClDeletedRecord as DeletedRecord
 from cl.runtime.storage.cl_data_class_record import ClDataClassRecord as DataClassRecord
 from cl.runtime.storage.cl_data_source_key import ClDataSourceKey as DataSourceKey
 from cl.runtime.storage.cl_data_source import ClDataSource as DataSource
---
+++ cl/runtime/storage/cl_deleted_record.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
+from cl.runtime.storage.cl_record import ClRecord
+
+
+@dataclass
+class ClDeletedRecord(ClRecord):
+    """
+    Represents a deleted record in commit queue and in those
+    cases when the database is write-once, or to shadow records
+    in parent datasets or data sources from lookup.
+    """
+
+    pk: Optional[str] = None
+    """Primary key (PK) string."""
+
+    def to_pk(self) -> str:
+        """Return primary key (PK) as string."""
+        return self.pk
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Serialize self as dictionary (may return shallow copy)."""
+        raise RuntimeError(f'Attempting to serialize a deleted record.')
+
+    def from_dict(self, data: Dict[str, Any]) -> None:
+        """Populate self from dictionary (must perform deep copy)."""
+        raise RuntimeError(f'Attempting to deserialize a deleted record.')
