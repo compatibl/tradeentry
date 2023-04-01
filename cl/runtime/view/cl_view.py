f99d14b9bdb24e568de74810a80e3382185c64d4
Alexander Sokol <sokol@compatibl.com>
2023-04-01 00:45:15 -0400

    Added View and two derived types.
    (cherry picked from commit 10d7e9da93ad4a6cc9ffceeb5b7a6431806e965d)

---
+++ cl/runtime/view/cl_view.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from cl.runtime.view.cl_view_key import ClViewKey
+
+
+@dataclass
+class ClView(ClViewKey):
+    """
+    The data shown alongside the record in the front end.
+
+    When the record is displayed, the user interface backend
+    will run a query for the view_for field where the value
+    is primary key of the record for which the view is specified,
+    and will display each View returned by the query on a separate
+    tab or panel next to the record itself.
+    """
+
---
+++ cl/runtime/view/cl_view_key.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Optional
+import cl.runtime as rt
+
+
+@dataclass
+class ClViewKey(rt.DataClassRecord):
+    """
+    The data shown alongside the record in the front end.
+
+    When the record is displayed, the user interface backend
+    will run a query for the view_for field where the value
+    is primary key of the record for which the view is specified,
+    and will display each View returned by the query on a separate
+    tab or panel next to the record itself.
+    """
+
+    view_for: Optional[str] = None
+    """Primary key of the record for which the view is specified."""
+
+    view_name: Optional[str] = None
+    """Name of the view displayed in the front end."""
+
+    def to_pk(self) -> str:
+        """Return primary key (PK) as string."""
+        return f'rt.View;{self.view_for};{self.view_name}'
