524ca24ab61cb2caf520662ff5b15faec004ba6d
Alexander Sokol <sokol@compatibl.com>
2023-03-31 22:11:06 -0400

    Add project URLs.
    (cherry picked from commit 9b664dfd3d010b9f43627f9568900994a45ead41)

--- setup.py
+++ setup.py
@@ -14,7 +14,11 @@ setuptools.setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     install_requires=requirements,
-    packages=setuptools.find_namespace_packages(where='.', include=['cl.runtime*']),
+    url='https://github.com/compatibl/cl-runtime',
+    project_urls={
+        'Source Code': 'https://github.com/compatibl/cl-runtime',
+    },
+    packages=setuptools.find_namespace_packages(where='.', include=['cl.runtime*'], exclude=['tests', 'tests.*']),
     package_dir={'': '.'},
     classifiers=[
         # Alpha - will attempt to avoid breaking changes but they remain possible
@@ -23,8 +27,7 @@ setuptools.setup(
         # Audience and topic
         'Intended Audience :: Developers',
         'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Scientific/Engineering',
 
         # License
         "License :: OSI Approved :: Apache Software License",
