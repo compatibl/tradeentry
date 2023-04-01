13fbddf4138c775d07ff1963533c76d7096e6a63
Alexander Sokol <sokol@compatibl.com>
2023-04-01 00:39:10 -0400

    Add minimal setup.py.
    (cherry picked from commit 104853dda189927d62e252eaef34a456720104ef)

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('./README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+with open('./requirements.txt') as requirements_file:
+    requirements = [line.strip() for line in requirements_file.readlines()]
+
+setuptools.setup(
+    name='cl-runtime',
+    version='1.0.0',
+    author='The Project Contributors',
+    description='CompatibL Runtime Community Edition',
+    long_description=readme
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    packages=setuptools.find_namespace_packages(where='.', include=['cl.runtime*']),
+    package_dir={'': '.'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: Apache Software License",
+        'Operating System :: OS Independent',
+    ],
+)
