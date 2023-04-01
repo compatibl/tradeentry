a5fe8a98400783e45aecf938aabfc25c0e5e1f32
Alexander Sokol <sokol@compatibl.com>
2023-03-31 22:05:34 -0400

    Add code to script to delete build artifacts except the wheel itself.
    (cherry picked from commit f8445eebb39618f8598be97a20a2d4054a9f1efb)

--- scripts/build_wheel.cmd
+++ scripts/build_wheel.cmd
@@ -7,14 +7,19 @@ echo Activate virtual environment
 call venv\Scripts\activate.bat
 
 echo.
-echo Delete dist, build and egg-info directories
+echo Delete build, egg-info, and dist directories before building
 if exist cl_runtime.egg-info rmdir/q/s cl_runtime.egg-info
-if exist dist rmdir/q/s dist
 if exist build rmdir/q/s build
+if exist dist rmdir/q/s dist
 
 echo.
 echo Build wheel
 python setup.py bdist_wheel
+
+echo.
+echo Delete build and egg-info directories after building, wheel is located in dist
+if exist cl_runtime.egg-info rmdir/q/s cl_runtime.egg-info
+if exist build rmdir/q/s build
 
 echo.
 echo Exit without deactivating virtual environment
