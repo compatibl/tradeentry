ff68aeaa03bf2cefc0744fdcef3a0684cebaaea2
Alexander Sokol <sokol@compatibl.com>
2023-10-15 01:14:16 -0400

    Create separate linter and formatter scripts.
    (cherry picked from commit 13c54f93825e0378168077f1a924b5432fa530d4)

--- scripts/format_and_validate_source.cmd
+++ scripts/format_and_validate_source.cmd
@@ -3,14 +3,6 @@
 pushd ..
 
 echo.
-echo Activate virtual environment
-call venv\Scripts\activate.bat
-
-echo.
-echo Install linter requirements
-pip install -r requirements.linter.txt
-
-echo.
 echo Format using isort
 isort cl --sp=.isort.cfg
 isort tests --sp=.isort.cfg
@@ -19,13 +11,5 @@ echo.
 echo Format using black
 black -q cl --config=pyproject.toml
 black -q tests --config=pyproject.toml
-
-echo.
-echo Validate using flake8
-flake8 cl --config=.flake8
-flake8 tests --config=.flake8
-
-echo.
-echo Exit without deactivating virtual environment
 
 popd
---
+++ scripts/linters.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+pushd ..
+
+echo.
+echo Validate using flake8
+flake8 cl --config=.flake8
+flake8 tests --config=.flake8
+
+popd
