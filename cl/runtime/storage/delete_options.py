5a76b8af1f437270bcf2561c79870155d3c5d3f4
Alexander Sokol <sokol@compatibl.com>
2024-01-07 10:19:44 -0500

    Add load, save and delete options classes.

---
+++ cl/runtime/storage/delete_options.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.runtime.data.attrs.attrs_data_util import attrs_data
+from cl.runtime.data.data import Data
+
+
+@attrs_data
+class DeleteOptions(Data):
+    """Optional flags for delete methods."""
---
+++ cl/runtime/storage/load_options.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.runtime.data.attrs.attrs_data_util import attrs_data
+from cl.runtime.data.attrs.attrs_field_util import attrs_field
+from cl.runtime.data.data import Data
+
+
+@attrs_data
+class LoadOptions(Data):
+    """Optional flags for load methods."""
+
+    none_if_not_found: bool = attrs_field()
+    """Return None instead of error message if the record is not found."""
+
+    none_if_no_key: bool = attrs_field()
+    """
+    Return None instead of error message if key=None is passed as argument.
+
+    This option can be used to simplify the code for loading a record
+    from a key stored in an optional element.
+    """
