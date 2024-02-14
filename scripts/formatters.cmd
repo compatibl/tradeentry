dffe010c3e379cc70f59af8a8aa95e4287c29591
Alexander Sokol <sokol@compatibl.com>
2024-02-14 02:15:04 -0500

    Add stubs directory to linter and formatter scripts.

--- scripts/formatters.cmd
+++ scripts/formatters.cmd
@@ -5,11 +5,13 @@ pushd ..
 echo.
 echo Format using isort
 isort cl --sp=.isort.cfg
+isort stubs --sp=.isort.cfg
 isort tests --sp=.isort.cfg
 
 echo.
 echo Format using black
 black -q cl --config=pyproject.toml
+black -q stubs --config=pyproject.toml
 black -q tests --config=pyproject.toml
 
 popd
