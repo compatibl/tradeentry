aafc3cc6157552d3ca06cd7c4eb1e2b01f4b94f8
Alexander Sokol <sokol@compatibl.com>
2024-04-12 18:38:56 -0400

    Update query and order fields in data source API, improve comments.

--- cl/runtime/storage/data_source.py
+++ cl/runtime/storage/data_source.py
@@ -31,48 +31,58 @@ class DataSource(DataSourceKey, RecordMixin, ABC):
     def load_many(
         self,
         table: str,
-        keys: Iterable[Dict[str, Any] | str],
+        keys: Iterable[Dict[str, Any] | str | None],
         dataset: List[str] | str | None = None,
     ) -> Iterable[Dict[str, Any] | None]:
         """
-        Load records serialized into dicts from the specified table using a list of keys.
+        Load serialized records from a single table using a list of keys.
+        
+        Returns:
+            Iterable with the same length and in the same order as the list of keys containing dicts.
+            where each level is a supported primitive type, list, or another dict.
+            A result element is None if the record is not found or the key is None.
 
         Args:
-            table: Table from which the records will be deleted
-            keys: Each key is either a dictionary of primary key fields or semicolon-delimited string
-            dataset: List of datasets in lookup order, single dataset, or None for root dataset
+            table: Table from which the records will be loaded.
+            keys: Each element is either a dictionary of primary key fields or semicolon-delimited string.
+            dataset: List of datasets in lookup order, single dataset, or None for root dataset.
         """
 
     @abstractmethod
     def load_by_query(
         self,
         table: str,
-        query: Dict[str, Any],
+        query: Dict[str, Any] | None,
+        order: Dict[str, int] | None = None,
         dataset: List[str] | str | None = None,
     ) -> Iterable[Dict[str, Any]]:
         """
-        Load records serialized into dicts from the specified table using a query.
+        Load serialized records from a single table by query.
+        
+        Returns:
+            Iterable containing dicts where each level is a supported primitive type, list, or another dict.
 
         Args:
-            table: Table from which the records will be deleted
-            query: Dictionary of conditions
-            dataset: List of datasets in lookup order, single dataset, or None for root dataset
+            table: Table from which the records will be loaded.
+            query: NoSQL query as dict in MongoDB format, or None to load all records from the table.
+            order: NoSQL sorting order in MongoDB format, or None if the result can be in any order.
+            dataset: List of datasets in lookup order, single dataset, or None for root dataset.
         """
 
     @abstractmethod
     def save_many(
         self,
         table: str,
-        records: Iterable[Dict[str, Any]],
+        records: Iterable[Dict[str, Any] | None],
         dataset: List[str] | str | None = None,
     ) -> None:
         """
-        Save records serialized as dicts into the specified table (overwrite if already exists).
+        Save serialized records to a single table (overwrite records that already exist).
 
         Args:
-            table: Table to which the records will be saved
-            records: Each serialized into dict where each level is primitive type, dict, or list.
-            dataset: List of datasets in lookup order, single dataset, or None for root dataset
+            table: Table to which the records will be saved.
+            records: Iterable of dicts where each level is a supported primitive type, list, or another dict.
+            dataset: List of datasets in lookup order, single dataset, or None for root dataset.
         """
 
     @abstractmethod
@@ -86,9 +96,9 @@ class DataSource(DataSourceKey, RecordMixin, ABC):
         Delete records in the specified table using a list of keys (no error if does not exist).
 
         Args:
-            table: Table from which the records will be deleted
-            keys: Each key is either a dictionary of primary key fields or semicolon-delimited string
-            dataset: List of datasets in lookup order, single dataset, or None for root dataset
+            table: Table from which the records will be deleted.
+            keys: Each key is either a dictionary of primary key fields or semicolon-delimited string.
+            dataset: List of datasets in lookup order, single dataset, or None for root dataset.
         """
 
     @abstractmethod
