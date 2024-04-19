d44f482322840d1cbc33f43c41e47531539b80f7
Alexander Sokol <sokol@compatibl.com>
2024-04-19 17:24:32 -0400

    Create a separate ContextMixin class for context methods with standard implementation.

---
+++ cl/runtime/rest/context_mixin.py
@@ -0,0 +1,105 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from logging import Logger
+from typing import List, TypeVar
+from cl.runtime.storage.data_source import DataSource
+from cl.runtime.rest.progress import Progress
+
+
+class ContextMixin:
+    """Provides logging, data source, dataset, and progress reporting."""
+
+    def save_on_commit(self, record: RecordMixin, dataset: str) -> None:
+        """
+        Add the record to the commit queue.
+
+        The record will not be saved until a call to commit() which
+        executes all pending requests in the commit queue. Alternatively,
+        rollback() may be used to clear the commit queue without executing
+        the pending requests.
+        """
+        raise NotImplementedError()
+
+    def delete_many(self, keys: Iterable[RecordMixin], dataset: str) -> None:
+        """
+        Delete many records in the specified dataset, bypassing
+        the commit queue. If an element of the 'keys' argument is
+        a full record, only its key will be used.
+
+        This method does not implicitly call commit(). The commit queue
+        will remain in its original state after the method exits.
+
+        Depending on data source implementation, this method may
+        delete a record or write the delete marker (DeletedRecord)
+        to shadow previous versions of the record or records with
+        the same key in a parent dataset. This ensures that records
+        with the same primary key in dataset and/or data source lookup
+        chain do not become visible after this record is deleted.
+        """
+        raise NotImplementedError()
+
+    def delete_on_commit(self, key: RecordMixin, dataset: str) -> None:
+        """
+        Add to commit queue the command to delete record in the
+        specified dataset. No error is raised if the record does not
+        exist. If the 'key' argument is a full record, only its
+        key will be used.
+
+        The record will not be saved until a call to commit() which
+        executes all pending requests in the commit queue. Alternatively,
+        rollback() may be used to clear the commit queue without executing
+        the pending requests.
+
+        Depending on data source implementation, this method may
+        delete a record or write the delete marker (DeletedRecord)
+        to shadow previous versions of the record or records with
+        the same key in a parent dataset. This ensures that records
+        with the same primary key in dataset and/or data source lookup
+        chain do not become visible after this record is deleted.
+        To avoid an additional roundtrip to the data store, the delete
+        marker may be written even when the record does not exist.
+        """
+        raise NotImplementedError()
+
+    def commit(self) -> None:
+        """
+        Execute all pending save and delete requests in the commit queue
+        and clear the queue.
+        """
+        raise NotImplementedError()
+
+    def rollback(self) -> None:
+        """
+        Clear the commit queue without executing the pending save and delete
+        requests in the queue.
+        """
+        raise NotImplementedError()
+
+    def delete_db(self) -> None:
+        """
+        Permanently deletes (drops) the database with all records
+        in it without the possibility to recover them later.
+
+        This method should only be used to free storage. For
+        all other purposes, methods that preserve history should
+        be used.
+
+        ATTENTION - THIS METHOD WILL DELETE ALL DATA WITHOUT
+        THE POSSIBILITY OF RECOVERY. USE WITH CAUTION.
+        """
+        raise NotImplementedError()
