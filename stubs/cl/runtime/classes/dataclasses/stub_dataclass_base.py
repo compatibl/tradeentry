92aeb866ec4df029e42ce490175fa7757cce3e80
Alexander Sokol <sokol@compatibl.com>
2024-04-19 17:10:56 -0400

    Add stub dataclass record.

---
+++ stubs/cl/runtime/classes/dataclasses/stub_dataclass_base.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Tuple
+from cl.runtime.classes.record_mixin import RecordMixin
+
+
+@dataclass
+class StubDataclassBase(RecordMixin):
+    """Stub record base class."""
+
+    str_field: str = field(default="abc")
+    """Stub field."""
+
+    int_field: int = field(default=123)
+    """Stub field."""
+
+    def get_key(self) -> Tuple:
+        """Return key as tuple in (BaseClass, key_field_1, key_field_2, ...) format."""
+        return StubDataclassBase, self.str_field, self.int_field
