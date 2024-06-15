92461e4a38b9c95ae1fde0282f7cf1bac35b1c49
Alexander Sokol <sokol@compatibl.com>
2024-06-15 18:35:03 -0400

    Compare type declaration dictionaries rather than classes.

--- tests/cl/runtime/schema/test_get_schema.py
+++ tests/cl/runtime/schema/test_get_schema.py
@@ -12,52 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import dataclasses
 import json
 import os
 import pytest
 from cl.runtime.schema.dataclasses.dataclass_type_decl import DataclassTypeDecl
-from cl.runtime.schema.type_decl import TypeDecl
-from inflection import titleize
 from stubs.cl.runtime import StubDataclassNestedFields
 from stubs.cl.runtime import StubDataclassRecord
 from stubs.cl.runtime.records.dataclasses.stub_dataclass_optional_fields import StubDataclassOptionalFields
-from typing import Any
-from typing import Dict
-from typing import Type
 
 
-def get_type_decl(cls: Type) -> Dict[str, Any]:
-    """Get type declaration for a class."""
-
-    type_decl = {
-        "module": {"module_name": cls.__module__},
-        "name": cls.__name__,
-        "label": titleize(cls.__name__),
-        "comment": cls.__doc__ or "",
-        "kind": "Element",
-        "display_kind": "Basic",
-        "elements": elements,
-        "keys": key_fields,
-    }
-
-    return type_decl
+def clean_dict(d):
+    if isinstance(d, dict):
+        return {k: clean_dict(v) for k, v in d.items() if v not in [None, False]}
+    elif isinstance(d, list):
+        return [clean_dict(i) for i in d if i not in [None, False]]
+    else:
+        return d
 
 
 def test_method():
     """Test coroutine for /schema/typeV2 route."""
 
-    sample_types = [StubDataclassRecord, StubDataclassOptionalFields, StubDataclassNestedFields]
+    sample_types = [StubDataclassRecord] # , StubDataclassOptionalFields, StubDataclassNestedFields]
 
     for sample_type in sample_types:
         class_module = sample_type.__module__.rsplit(".", maxsplit=1)[1]
         expected_result_file_path = os.path.abspath(__file__).replace(".py", f".{class_module}.expected.json")
         with open(expected_result_file_path, "r", encoding="utf-8") as file:
-            expected_result = json.load(file)
+            expected_result_dict = json.load(file)
 
-        expected_result_obj = DataclassTypeDecl(**expected_result)
         result_obj = DataclassTypeDecl.for_type(sample_type)
+        result_dict = clean_dict(dataclasses.asdict(result_obj))
+        assert result_dict == expected_result_dict
         pass
-        # TODO: Compare assert result_obj == expected_result_obj
 
 
 if __name__ == "__main__":
