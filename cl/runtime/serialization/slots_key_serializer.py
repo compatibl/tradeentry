057c4c0ac0eaf55ffc45d6e6241bdc055e269fd0
Alexander Sokol <sokol@compatibl.com>
2024-07-06 15:28:19 -0400

    Add KeySerializer for serializing key into tuple.

---
+++ cl/runtime/serialization/slots_key_serializer.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+
+primitive_type_names = ["NoneType", "str", "float", "int", "bool", "date", "time", "datetime", "bytes", "UUID"]
+"""Detect primitive type by checking if class name is in this list."""
+
+
+# TODO: Add checks for to_node, from_node implementation for custom override of default serializer
+class SlotsKeySerializer:
+    """Serialize key."""
+
+    def serialize_key(self, data):
+        """Serialize key."""
+
+        key_slots = data.get_key_type().__slots__
+        return tuple(v if (v := getattr(data, k)).__class__.__name__ in primitive_type_names else v.name if isinstance(v, Enum) else self.serialize_key(v) for k in key_slots)
+
---
+++ tests/cl/runtime/serialization/test_slots_key_serializer.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from cl.runtime.serialization.slots_key_serializer import SlotsKeySerializer
+from stubs.cl.runtime import StubDataclassNestedFields, StubDataclassListFields, StubDataclassPrimitiveFields
+from stubs.cl.runtime import StubDataclassRecord
+from stubs.cl.runtime import StubDataclassOptionalFields
+
+
+def test_smoke():
+    """Test coroutine for /schema/typeV2 route."""
+
+    sample_types = [
+        StubDataclassRecord,
+        StubDataclassPrimitiveFields,
+        StubDataclassListFields,
+        StubDataclassNestedFields,
+        StubDataclassOptionalFields
+    ]
+
+    key_serializer = SlotsKeySerializer()
+
+    for sample_type in sample_types:
+
+        obj_1 = sample_type()
+        serialized_1 = key_serializer.serialize_key(obj_1)
+        serialized_2 = key_serializer.serialize_key(obj_1.get_key())
+
+        assert serialized_1 == serialized_2
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
