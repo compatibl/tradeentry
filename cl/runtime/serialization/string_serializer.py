d36d0995c9dbbe1655757d2ed0e1c3ecb0496246
Roman Kushnir <romank@compatibl.com>
2024-08-01 09:22:02 -0700

    Added serialize key token methods.
    (cherry picked from commit a9f5993677d93e10fdf45035114437476f5ccb9f)
    (cherry picked from commit 48f31debd6b276c689c2f165adeb6c64443220fe)

--- cl/runtime/serialization/string_serializer.py
+++ cl/runtime/serialization/string_serializer.py
@@ -14,8 +14,10 @@
 
 from enum import Enum
 from typing import Any, Type
+import datetime as dt
 
 from cl.runtime.records.protocols import KeyProtocol
+from cl.runtime.serialization.string_value_parser import StringValueParser, StringValueCustomType
 from cl.runtime.storage.data_source_types import TDataset
 
 primitive_type_names = ["NoneType", "str", "float", "int", "bool", "date", "time", "datetime", "bytes", "UUID"]
@@ -47,15 +49,61 @@ class StringSerializer:
             raise RuntimeError(f"Invalid dataset or its token {dataset}. Valid token types are None, "
                                f"primitive types, enum or their iterables.")
 
+    def _serialize_key_token(self, data) -> str:
+
+        if data is None:
+            return ''
+
+        if isinstance(data, str):
+            return data
+
+        value_custom_type = StringValueParser.get_custom_type(data)
+
+        if value_custom_type in [StringValueCustomType.data, StringValueCustomType.dict, StringValueCustomType.list]:
+            raise ValueError(f"Value {str(data)} of type {type(data)} is not supported in key.")
+
+        if value_custom_type in [
+            StringValueCustomType.date, StringValueCustomType.datetime, StringValueCustomType.time
+        ]:
+            result = data.isoformat()
+        elif value_custom_type == StringValueCustomType.enum:
+            result = f"{type(data).__name__}.{data.name}"
+        else:
+            result = str(data)
+
+        return StringValueParser.add_type_prefix(result, value_custom_type)
+
+    def _deserialize_key_token(self, data: str) -> Any:
+
+        value, value_custom_type = StringValueParser.parse(data)
+
+        if value_custom_type is None:
+            return value if value != '' else None
+
+        if value_custom_type == StringValueCustomType.date:
+            return dt.date.fromisoformat(value)
+        elif value_custom_type == StringValueCustomType.datetime:
+            return dt.datetime.fromisoformat(value)
+        elif value_custom_type == StringValueCustomType.time:
+            return dt.time.fromisoformat(value)
+        elif value_custom_type == StringValueCustomType.bool:
+            return True if value.lower() == "true" else False
+        elif value_custom_type == StringValueCustomType.int:
+            return int(value)
+        elif value_custom_type == StringValueCustomType.float:
+            return float(value)
+        elif value_custom_type == StringValueCustomType.enum:
+            enum_type, enum_value = value.split(".")
+        else:
+            return value
+
     def serialize_key(self, data):
         """Serialize key to string, flattening for composite keys."""
 
         key_slots = data.get_key_type().__slots__
         result = ";".join(
             str(v)  # TODO: Apply rules depending on the specific primitive type
-            if (v := getattr(data, k)).__class__.__name__ in primitive_type_names
-            else v.name
-            if isinstance(v, Enum)
+            if (v := getattr(data, k)).__class__.__name__ in primitive_type_names or isinstance(v, Enum)
             else self.serialize_key(v)
             for k in key_slots
         )
@@ -74,5 +122,5 @@ class StringSerializer:
         filled_slots = key_slots[:len(key_tokens)]
 
         # TODO: deserialize string tokens using specific rules (or annotations?)
-        key_fields = {slot: token for slot, token in zip(filled_slots, key_tokens)}
+        key_fields = {slot: self._deserialize_key_token(token) for slot, token in zip(filled_slots, key_tokens)}
         return key_type(**key_fields)
