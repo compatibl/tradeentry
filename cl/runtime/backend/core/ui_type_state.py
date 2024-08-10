aa0b9de0cb2c5bde2f56956c0259343f80d336bb
Alexander Sokol <sokol@compatibl.com>
2024-08-10 16:18:25 -0400

    Update copyright header.

--- cl/runtime/backend/core/ui_type_state.py
+++ cl/runtime/backend/core/ui_type_state.py
@@ -1,8 +1,16 @@
-# Copyright (C) 2003-present CompatibL. All rights reserved.
+# Copyright (C) 2023-present The Project Contributors
 #
-# This file contains valuable trade secrets and may be copied, stored, used,
-# or distributed only in compliance with the terms of a written commercial
-# license from CompatibL and with the inclusion of this copyright notice.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from cl.runtime.backend.core.ui_type_state_key import UiTypeStateKey
 from cl.runtime.records.dataclasses_extensions import field
