70b3b6bd880f9e528101c7fb0fe2efa819b9af27
Roman Kushnir <romank@compatibl.com>
2024-08-20 10:48:34 -0700

    Added test coroutine for /entity/panel route.
    (cherry picked from commit 01c66b69a93f8d240b7803002e23f3fecb63a065)

---
+++ tests/cl/runtime/routers/entity/test_panel.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.runtime.context.context import current_or_default_data_source
+from cl.runtime.routers.entity.panel_request import PanelRequest
+from cl.runtime.routers.entity.panel_response_util import PanelResponseUtil
+from cl.runtime.serialization.string_serializer import StringSerializer
+from stubs.cl.runtime.decorators.stub_viewers import StubViewers
+
+# create stub with viewers
+stub_viewers = StubViewers()
+key_serializer = StringSerializer()
+key_str = key_serializer.serialize_key(stub_viewers.get_key())
+
+
+requests = [
+    {"type": "StubViewers", "panel_id": "Instance Viewer 1A", "key": key_str},
+    {"type": "StubViewers", "panel_id": "Instance Viewer 1B", "key": key_str},
+    {"type": "StubViewers", "panel_id": "Instance Viewer 1C", "key": key_str},
+]
+
+expected_results = [
+    {
+        "ViewOf": {
+            "_t": "Script",
+            "Name": None,
+            "Language": "Markdown",
+            "Body": ["# Viewer with UI element", "### _Script_"],
+            "WordWrap": None
+        }
+    },
+    {"ViewOf": None},
+    {"ViewOf": {"StubId": key_str, "_t": "StubViewersKey"}},
+]
+
+
+def test_method():
+    """Test coroutine for /entity/panel route."""
+
+    data_source = current_or_default_data_source()
+    try:
+        data_source.save_one(stub_viewers)
+
+        for request, expected_result in zip(requests, expected_results):
+            request_object = PanelRequest(**request)
+            result = PanelResponseUtil.get_content(request_object)
+
+            assert isinstance(result, dict)
+    finally:
+        data_source.delete_db()
