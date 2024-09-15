cf2c79296e01f26575f23bfd56dea7fc1374b184
Alexander Sokol <sokol@compatibl.com>
2024-09-15 13:04:42 -0400

    Add setup.py and requirements.
    (cherry picked from commit cd112054ba522add5b9382f0a38ead151c092137)

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open('./README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+with open('./install_requirements.txt') as install_requirements:
+    install_requires = [line.strip() for line in install_requirements.readlines()]
+
+setuptools.setup(
+    name='convince',
+    version='0.0.1',
+    author='The Project Contributors',
+    description='Better instruction following for large language models',
+    license='Apache Software License',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    url='https://github.com/compatibl/convince',
+    project_urls={
+        'Source Code': 'https://github.com/compatibl/convince',
+    },
+    packages=setuptools.find_packages(include=('convince', 'convince.*'), exclude=('tests', 'tests.*')),
+    classifiers=[
+        # Alpha - will attempt to avoid breaking changes but they remain possible
+        'Development Status :: 3 - Alpha',
+
+        # Audience and topic
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+
+        # License
+        'License :: OSI Approved :: Apache Software License',
+
+        # Runs on Python 3.10 and later releases
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+
+        # Operating system
+        'Operating System :: OS Independent',
+    ],
+)
