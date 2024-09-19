951df21689bf8a589f92b5da1911cf83b76fc8b5
Alexander Sokol <sokol@compatibl.com>
2024-09-19 00:04:07 -0400

    Implement BarPlot.

---
+++ cl/runtime/plots/bar_plot.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+
+class BarPlot:
+    layout_background = {
+        'paper_bgcolor': 'rgba(255,255,255,1)',
+        'plot_bgcolor': 'rgba(255,255,255,1)',
+    }
+
+    @staticmethod
+    def create(
+            data: pd.Series,
+            ticks: Optional[np.ndarray] = None,
+            x_text: Optional[str] = 'Experiment',
+            y_text: Optional[str] = 'Value'
+    ) -> go.Figure:
+        bars = go.Bar(
+            x=data.index,
+            y=data
+        )
+
+        # Combine both heatmaps into one figure
+        fig = go.Figure(data=bars)
+
+        # Set white background
+        fig.update_layout(BarPlot.layout_background)
+
+        # Custom ticks if provided
+        if ticks is not None:
+            y_min = np.nanmin(ticks)
+            y_max = np.nanmax(ticks)
+
+            for value in [y_min, y_max]:
+                fig.add_hline(y=value, opacity=0, showlegend=False)
+
+            fig.update_layout(yaxis=dict(tickvals=ticks))
+
+        # add custom xaxis title
+        fig.add_annotation(dict(font=dict(color="black", size=14),
+                                x=0.5,
+                                y=-0.15,
+                                showarrow=False,
+                                text=x_text,
+                                xref="paper",
+                                yref="paper"))
+
+        # add custom yaxis title
+        fig.add_annotation(dict(font=dict(color="black", size=14),
+                                x=-0.35,
+                                y=0.5,
+                                showarrow=False,
+                                text=y_text,
+                                textangle=-90,
+                                xref="paper",
+                                yref="paper"))
+
+        return fig
---
+++ tests/cl/runtime/plots/test_bar_plot.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from cl.runtime.plots.bar_plot import BarPlot
+from cl.runtime.testing.pytest.pytest_fixtures import local_dir_fixture
+
+
+def test_smoke(local_dir_fixture):
+    data = pd.Series({
+        'Model 1': 85.5,
+        'Model 2': 92,
+        'Model 3': 70,
+        'Model 4': 83.7
+    })
+
+    fig = BarPlot.create(
+        data=data,
+        ticks=np.arange(0, 101, 10)
+    )
+    fig.write_image("test_bar_plot.test_smoke.png")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
