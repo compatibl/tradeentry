c0eeb9fd78d61038ff9bbee6557d1606e41d4f8c
Alexander Sokol <sokol@compatibl.com>
2024-09-19 20:38:45 -0400

    Add prebuild tests.

---
+++ tests/cl/tradeentry/prebuild/test_init_files.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from pathlib import Path
+from cl.runtime.prebuild.init_files import check_init_files
+
+
+def test_init_files():
+    """Prebuild test to check that __init__.py is present in all code directories."""
+
+    # Check that init files are present in each source directory except for tests, report an error if not found
+    check_init_files(apply_fix=False)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
