92ceb4b006e95952c9a913bd1171288dc7172eec
Alexander Sokol <sokol@compatibl.com>
2024-09-24 11:19:33 -0400

    Update completion cache test for the new logic where the file is not read after cache object construction.

--- tests/cl/convince/llms/test_completion_cache.py
+++ tests/cl/convince/llms/test_completion_cache.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List
+
 import pytest
 import os
 from cl.convince.llms.completion_cache import CompletionCache
@@ -19,28 +21,39 @@ from cl.convince.llms.completion_cache import CompletionCache
 module_path = __file__.removesuffix(".py")
 
 
-def perform_testing(base_path: str, full: bool = False):
-    """Stub test function without a class."""
-
-    # Test channels, the first two are repeated to test writing from two separate objects
-    channels = ["channel.1", "channel.1", "channel.2"]
-
-    # Delete existing test cache files to prevent starting from previous test output
+def _delete_cache_files(base_path: str, channels: List[str]):
+    """Delete existing test cache files to prevent starting from previous test output or git diff at the end."""
     for unique_channel in set(channels):
         file_path = f"{base_path}.{unique_channel}.completions.csv"
         if os.path.exists(file_path):
             os.remove(file_path)
 
+
+def _perform_testing(base_path: str):
+    """Stub test function without a class."""
+
+    # Test channels, the first two are repeated to test writing from two separate objects
+    channels = ["channel.1", "channel.1", "channel.2"]
+
+    # Delete the existing test cache files to prevent starting from previous test output
+    _delete_cache_files(base_path, channels)
+
     # Perform testing, two of the cache files are duplicates
     caches = [CompletionCache(channel=channel) for channel in channels]
 
-    # Check that none are found befoe writing
+    # Check that none are found before writing
     assert all(cache.get("a") is None for cache in caches)
 
     # Write a to all files
     [cache.add("a", "b") for cache in caches]
 
-    # Check that all is found in all
+    # Check that still none are found (because the cache files are not read after construction)
+    assert all(cache.get("a") is None for cache in caches)
+
+    # Recreate caches, this will reload files from disk
+    caches = [CompletionCache(channel=channel) for channel in channels]
+
+    # Make sure "a" is found for all caches
     assert all(cache.get("a") == "b" for cache in caches)
 
     # Check that e is found in none
@@ -49,30 +62,39 @@ def perform_testing(base_path: str, full: bool = False):
     # Write c to the first file only
     caches[0].add("c", "d")
 
-    # Check that c is found only in the first one before reload (because the cache file is not read during the run)
-    assert caches[0].get("c") == "d"
+    # Check that c is not found (because the cache files are not read after construction)
+    assert caches[0].get("c") is None
     assert caches[1].get("c") is None
     assert caches[2].get("c") is None
 
-    # Reload and confirm it is visible to the first two now
+    # Recreate caches, this will reload files from disk
     caches = [CompletionCache(channel=channel) for channel in channels]
+
+    # Reload and confirm it is visible to the first two now
     assert caches[0].get("c") == "d"
     assert caches[1].get("c") == "d"
     assert caches[2].get("c") is None
 
     # Write another value for "a", the file should have both but the new value is returned by get
     caches[0].add("a", "bb")
+
+    # Recreate caches, this will reload files from disk
     caches = [CompletionCache(channel=channel) for channel in channels]
+
+    # Check for new values
     assert caches[0].get("a") == "bb"
     assert caches[1].get("a") == "bb"
     assert caches[2].get("a") == "b"
+
+    # Delete the generated test cache files to prevent git diff
+    _delete_cache_files(base_path, channels)
 
 
 def test_function():
     """Stub test function without a class."""
 
     # Test calling from a function
-    perform_testing(f"{module_path}.test_function", full=True)
+    _perform_testing(f"{module_path}.test_function")
 
 
 class TestClass:
@@ -82,7 +104,7 @@ class TestClass:
         """Stub test method inside pytest class."""
 
         # Test calling from a method
-        perform_testing(f"{module_path}.test_class.test_method")
+        _perform_testing(f"{module_path}.test_class.test_method")
 
 
 if __name__ == "__main__":
