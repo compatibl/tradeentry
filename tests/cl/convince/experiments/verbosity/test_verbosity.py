c22fb89355574f481d84416aa96da9020e89eabb
Alexander Sokol <sokol@compatibl.com>
2024-10-01 16:52:39 -0400

    Update convince experiments to use flat data format for plots.
    (cherry picked from commit ce10d12130df220fbf5dfd56e4b0282bc4ee63f5)

--- tests/cl/convince/experiments/verbosity/test_verbosity.py
+++ tests/cl/convince/experiments/verbosity/test_verbosity.py
@@ -43,6 +43,8 @@ def test_verbosity(local_dir_fixture):
         reps = 2
         plot = GroupBarPlot(plot_id="verbosity")
         plot.values = []
+        plot.bar_labels = []
+        plot.group_labels = []
         stub_mini_llms = get_stub_mini_llms()
         for llm in stub_mini_llms:
 
@@ -51,11 +53,11 @@ def test_verbosity(local_dir_fixture):
             extended_sum = sum(llm.completion(_get_extended_prompt(i + 1)) == str(pow(i + 1, 2)) for i in range(reps))
 
             # Add to plot
+            plot.bar_labels.extend([llm.llm_id] * 2)
+            plot.group_labels.extend(["Simple", "Extended"])
             plot.values.extend([simple_sum / reps, extended_sum / reps])
 
-        # Apply labels
-        plot.bar_labels = [llm.llm_id for llm in stub_mini_llms]
-        plot.group_labels = ["Simple", "Extended"]
+        # Save
         plot.save_png()
 
 
