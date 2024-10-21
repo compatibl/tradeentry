87e76d6cf913097c9b16e22d3d76381f2482e2cb
Alexander Sokol <sokol@compatibl.com>
2024-10-21 04:00:58 -0400

    Add tools and build files from runtime.

---
+++ tools/cl/runtime/fix_copyright_header_blank_line.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2023-present The Project Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cl.runtime.prebuild.copyright_header import check_copyright_headers
+
+# Check copyright headers and fix missing trailing blank line
+# All other copyright header errors cause an exception
+if __name__ == '__main__':
+
+    # Fix or report errors where copyright header is missing, incorrect, or not followed by a blank line
+    check_copyright_headers(fix_trailing_blank_line=True, verbose=True)
